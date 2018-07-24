dd1b0188fa81350dd8948161dbf9502bcb4a7904
Jack Storey <jstorey@Jacks-MacBook-Pro.local>
2018-07-24 16:08:15 +0200

    Commnenting.

--- CS1301 exercises/Hello world.py
+++ CS1301 exercises/Hello world.py
@@ -1,2 +1,9 @@
 print ('hello world')
 print("Hello, CS1301")
+
+
+print("This is line 1")
+print("This is line 2")
+print("This is line 3")
+print("This is line 4")
+pritn("This is line 5") #This line will not work as there is a function error. "pritn does not exist" 
